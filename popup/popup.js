87654ccd88180d653c38fbf1c107b5e58b1d3c2e
Abhilekh <abhilekhgautam119@gmail.com>
2021-12-01 22:19:52 +0545

    password generating algorithm added

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,26 @@
-let btn = document.querySelector(".btn");
-let pass = document.querySelector(".pass")
+var btn = document.querySelector(".btn");
+var pass = document.querySelector(".pass")
+
+function generate_pass(){
+ const specialKey = "!@#$%^&*()_+;'"
+ const ints = "1234567890"
+ const lower_alphabets = "abcdefghijklmnopqrstuvwxyz"
+ const upper_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ 
+ var password = "";
+
+ for(var i = 0 ; i < 5 ; i++){
+  password += lower_alphabets.charAt(Math.floor(Math.random() * lower_alphabets.length))
+  password += ints.charAt(Math.floor(Math.random() * ints.length))
+  password += specialKey.charAt(Math.floor(Math.random() * 4))
+  password += upper_alphabets.charAt(Math.floor(Math.random() * upper_alphabets.length))
+ }
+ return password 
+}
 
 btn.addEventListener('click',()=>{
- //Todo:Generate a new password.
+var password =  generate_pass();
+pass.innerText = password; 
 })
+
+pass.innerText = generate_pass()
